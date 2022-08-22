5689495250cd600be6989a2e57e675fb48e444b4
maxim55069633 <1246003314@qq.com>
2022-08-22 20:04:43 +0800

    version 1.1 Show the trail of mouse in random color

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # odin_Etch-A-Sketch
+
+This project helped me to hone my DOM manipulation skills. I encountered many interesting challenges:
+
+1. how to make the divs appear as a grid in a fixed size of square dynamically.
+
+Flex saved me. It can grow or shrink dynamically no matter what number I enter.
+
+2. QuerySelector did't work. The console.log reminded me that:```Uncaught TypeError: Cannot setproperties of null.```
+
+I checked my codes and failed to find obvious mistakes first. later on, I found queryselector didn't catch an element. I realized I forget to add a defer property to the javascript on the html file. When the html tags are not generated, the javascript naturally doesn't catch. The order of codes matters.
+
+3. (Unsolved) I can't find a way to generate the mix of random color and black.
+
+After some google search, I found a color-mix function. However, it is only supported by a few browser.
+Then I came up with a the subsitute plan using hsl color. When the colorlightness is set to 0, the color is black. However, the higher colorlightness is, it is closer to white, not the expected mix of random color and black. 
+ 
---
+++ scripts.js
@@ -0,0 +1,75 @@
+let BlackAddedColor=80;
+
+function changeUnitColor(e){
+
+
+    // version 1.1 hsl can generate 10 random color. However, 
+    // The first color should be its original color instead of white. So I decided to start from 80 in colorlightness.
+    // Everytime a uesr clicks, it decreases 8 in colorlightness.
+
+    //I also found a new method color-mix(). Unfortunately, most major browsers don't support it.
+
+
+    let randomHue = Math.floor(Math.random()*361);
+    let randomSaturation = Math.floor(Math.random()*101);
+    let blackAddedColor = `hsl(${randomHue}, ${randomSaturation}%, ${BlackAddedColor}%)`;
+    this.style.backgroundColor = blackAddedColor;
+    if(BlackAddedColor >0)
+    BlackAddedColor =BlackAddedColor-8;
+
+    // version 1.0 only a single color
+    // this.classList.add("newColor");
+}
+
+function createDivs(n)
+{
+
+    let containerDiv = document.querySelector(".container");
+    containerDiv.innerHTML=``;
+    for(let i=0; i<n; i++)
+    {
+        let rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+
+        for(let j=0; j<n; j++)
+        {
+            let unitDiv =document.createElement('div');
+            unitDiv.classList.add('unit');
+            // unitDiv.textContent=i*16+j+1;// for test purpose
+
+            rowDiv.appendChild(unitDiv);
+
+        }
+
+        containerDiv.appendChild(rowDiv);
+    }
+
+    //prepare to change the color
+    let myUnits = document.querySelectorAll('.unit');
+    myUnits.forEach(unit=> unit.addEventListener('mouseover', changeUnitColor)
+    );
+
+
+}
+
+
+createDivs(16);
+
+function changeDivsGrid()
+{
+
+    let numberPerSideForSquare=prompt("The number should be a positive integer between 1 and 100",10);
+    while( true )
+    {
+        if ( numberPerSideForSquare >= 1 && numberPerSideForSquare <= 100 )
+            break;
+        else 
+            numberPerSideForSquare=prompt("Please enter a legit input! The number should be a positive integer between 1 and 100",10);
+    }
+    createDivs(numberPerSideForSquare);
+    BlackAddedColor=80;
+}
+
+let myInstructionButton=document.getElementById("enterNumberForSqaure");
+myInstructionButton.addEventListener('click', changeDivsGrid);
+
